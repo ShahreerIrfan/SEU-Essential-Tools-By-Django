fcb687160f3ebc7f391b8c1a18ec29284430ba51
md shahreer irfan <mdshahreerirfan@gmail.com>
2024-09-24 19:38:22 +0600

    working on home page

---
+++ seu_ools/views.py
@@ -0,0 +1,15 @@
+
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from reportlab.lib.pagesizes import A4
+from reportlab.lib import colors
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.units import inch
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, TableStyle, Image
+from datetime import datetime
+import io
+import qrcode
+
+
+def home(request):
+    return render(request,"base.html")
